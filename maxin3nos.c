00a8be785a7f485ff65efe4ee10adf5a81be7f2c
Kunal Ananda Sagar <kunalsagar0615@gmail.com>
2025-06-12 13:03:31 +0530

    max in3 numbers

---
+++ maxin3nos.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c;
+    printf("enter 3 nos=");
+    scanf("%d%d%d",&a,&b,&c);
+    if(a>b&&a>c)
+    printf("%d is max ",a);
+    else if(b>a && b>c)
+    printf("%d is max",b);
+    else
+    printf("%d is max",c);
+}
