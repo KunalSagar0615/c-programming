6781a8801a82ca3d2e4a1d991bc82f179c535244
Kunal Ananda Sagar <kunalsagar0615@gmail.com>
2025-06-18 13:11:11 +0530

    recognize entity

--- ascii2.c
+++ ascii2.c
@@ -6,7 +6,7 @@ void main()
     scanf("%c",&ch);
     if((ch>='A' && ch<='Z') || (ch>='a' && ch<='z'))
         printf("%c is alphabet",ch);
-    else if(ch>='0' && ch<='9aa')
+    else if(ch>='0' && ch<='9')
         printf("%c is digit",ch);
     else
         printf("%c is special character",ch);
